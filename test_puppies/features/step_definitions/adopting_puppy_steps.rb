17d93a4cd7bcd5e48f727ff2229636b3258a17a2
amansinghb <a.singh.b@gmail.com>
2016-03-18 13:49:45 +0000

    fixed typo
    There was a typo which was causing the tests to fail. Fixed

--- test_puppies/features/step_definitions/adopting_puppy_steps.rb
+++ test_puppies/features/step_definitions/adopting_puppy_steps.rb
@@ -47,17 +47,16 @@ Then(/^I should see "([^"]*)"$/) do |expected|
   @browser.text.should include expected
 end
 
+
+
 Then(/^I should see "([^"]*)" as the name for line item (\d+)$/) do |name, line_item|
-  # cart_line_item(line_item.to_i)[1].text.should include name
-  @car.name_for_line_item(line_item.to_i).should include name
+  @cart.name_for_line_item(line_item.to_i).should include name
 end
 
 Then(/^I should see "([^"]*)" as the subtotal for line item (\d+)$/) do |subtotal, line_item|
-  # cart_line_item(line_item.to_i)[3].text.should ==subtotal
   @cart.subtotal_for_line_item(line_item.to_i).should == subtotal
 end
 
 Then(/^I should see "([^"]*)" as the cart total$/) do |total|
-  # @browser.td(:class => 'total_cell').text.should == total
   @cart.cart_total.should == total
 end
